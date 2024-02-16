1680b5beabc43cca6aafaf3b4491c4b5c88dfc39
brain hamrage <ayajansari2002@gmail.com>
2024-02-17 00:00:39 +0530

    home page image fixed

--- Travel_Experience_Sharing_Platform/src/pages/Home.jsx
+++ Travel_Experience_Sharing_Platform/src/pages/Home.jsx
@@ -27,20 +27,20 @@ function Home(){
         
 
             
-            <div className="w-full pt-72 pb-56 lg:pb-60 xl:pb-72  lg:bg-cover  sm:bg-center  bg-[#006494]   bg-[url('/src/assets/landing1.png')]" > 
-                <div className="w-2/3 md:w-9/12 px-auto mx-auto lg:mr-auto lg:ml-40 flex flex-col lg:items-start items-center">
+            <div className="w-full pt-64 pb-64    bg-sky-100 bg-cover  bg-[url('src/assets/landing4.png')] " > 
+                <div className="flex flex-col w-2/3 lg:w-2/4 mx-auto">
 
             
-                    <h1 className="text-2xl text-center lg:text-start lg:text-4xl  lg:w-8/12  md:text-4xl md:w-8/12 sm:text-3xl sm:w-8/12  text-white font-bold">Share Your Journeys, Connect with Travelers</h1>
-                    <h3 className="text-lg w-3/4 lg:text-start text-center  py-4 text-[#c5d5e1]">Discover, share, and connect with fellow travelers on our vibrant platform, </h3>
-                    <div className="flex lg:justify-start justify-center pt-2 ">
+                    <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold text-white text-center" >Share Your Journeys, Connect with Travelers</h1>
+                    <h3 className=" text-center text-lg mt-4 mx-3/4 text-slate-300">Discover, share, and connect with fellow travelers on our vibrant platform. </h3>
+                    <div className="flex justify-center mt-6 ">
                         <Link to="/login">
-                            <div className={`w-24 px-3  py-2  text-lg  font-semibold  bg-white text-center text-black  rounded-sm `}  > 
+                            <div className={`w-24 px-3  py-2  text-lg  font-semibold  bg-[#006494] hover:bg-[#006594c3] text-center text-white  rounded-sm `}  > 
                                 Sign Up
                             </div>
                         </Link>
                         <Link to="/signup">
-                            <div className={`w-24 px-3 ml-4 py-2 text-lg font-semibold border border-white text-center text-white   rounded-sm `}  > 
+                            <div className={`w-24 px-3 ml-4 py-2 text-lg font-semibold border border-[#006494] text-center text-[#006494]   rounded-sm `}  > 
                                 Log In
                             </div>
                         </Link> 
