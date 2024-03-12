90f340290443824690c00016b1402796ef3dbb07
brain hamrage <ayajansari2002@gmail.com>
2024-03-13 00:50:58 +0530

    edit profile page completed

--- Travel_Experience_Sharing_Platform/src/pages/Edit.jsx
+++ Travel_Experience_Sharing_Platform/src/pages/Edit.jsx
@@ -9,73 +9,76 @@ function Edit(){
 
     }
     return (
-        <div className='bg-[#F7F8FA] w-full  py-16 px-4 sm:px-16 md:px-28  lg:px-52 xl:px-80'>
-            <p className='text-xl pb-8 text-[#757575]' >username/edit</p>
 
-            <form onSubmit={handleSubmit(handleSave)}>
-                <div className='border bg-white border-[#00000019] px-8 py-8 rounded-md'> 
-                    <InputField 
-                        type="text"
-                        placeholder="Username"
-                        className="w-full px-4 py-3   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
-                        {...register("username")}  
-                    />
-                    <InputField 
-                        type="email"
-                        placeholder="Email"
-                        className="w-full px-4 py-3 my-4   border focus:outline-none    rounded-md " 
-                        readOnly={true}
-                    />
-                    <textarea 
-                        name="about" 
-                        id="about"  
-                        rows="10" 
-                        placeholder='About' 
-                        readOnly={false} 
-                        className='w-full px-4 py-3 border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md'
-                        {...register("about")}
-                    >
-                        
-                    </textarea>
-                    <div className='w-full px-4 p-4 my-3   border  rounded-md'>
-                        <h1 className='text-lg text-[#9CA3AF]'>Social Media Links</h1>
-                        <div className=' flex items-center py-3 '>                            
-                            <img src="/src/assets/instagram.png " alt="" className='w-8 h-8' />
-                            <InputField 
-                                type="text"
-                                placeholder="Instagram URL"
-                                className="w-full px-4 py-1.5 ml-4   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
-                                {...register("instagram_link")}  
-                            />
+        <div className='bg-[#F7F8FA]'>
+            <p className='text-xl  text-[#757575] pt-12 px-8 md:px-24' >username/edit</p>
+            <div className=' w-full  py-16 px-4 sm:px-16 md:px-28  lg:px-52 xl:px-80 '>
+
+                <form onSubmit={handleSubmit(handleSave)}>
+                    <div className=' bg-white  px-8 py-8 rounded-md shadow-xl'> 
+                        <InputField 
+                            type="text"
+                            placeholder="Username"
+                            className="w-full px-4 py-3   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
+                            {...register("username")}  
+                        />
+                        <InputField 
+                            type="email"
+                            placeholder="Email"
+                            className="w-full px-4 py-3 my-4   border focus:outline-none    rounded-md " 
+                            readOnly={true}
+                        />
+                        <textarea 
+                            name="about" 
+                            id="about"  
+                            rows="10" 
+                            placeholder='About' 
+                            readOnly={false} 
+                            className='w-full px-4 py-3 border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md'
+                            {...register("about")}
+                        >
+                            
+                        </textarea>
+                        <div className='w-full px-6 pt-4 pb-6 my-3   border  rounded-md'>
+                            <h1 className='text-lg text-[#9CA3AF]'>Social Media Links</h1>
+                            <div className=' flex items-center py-3 '>                            
+                                <img src="/src/assets/instagram.png " alt="" className='w-8 h-8' />
+                                <InputField 
+                                    type="text"
+                                    placeholder="Instagram URL"
+                                    className="w-full px-4 py-1.5 ml-4   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
+                                    {...register("instagram_link")}  
+                                />
+                            </div>
+                            <div className=' flex items-center py-3'>                            
+                                <img src="/src/assets/facebook.png " alt="" className='w-8 h-8' />
+                                <InputField 
+                                    type="text"
+                                    placeholder="Facebook URL"
+                                    className="w-full px-4 py-1.5 ml-4   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
+                                    {...register("facebook_link")}  
+                                />
+                            </div>
+                            <div className=' flex items-center py-3'>                            
+                                <img src="/src/assets/twitter.png " alt="" className='w-8 h-8' />
+                                <InputField 
+                                    type="text"
+                                    placeholder="Twitter URL"
+                                    className="w-full px-4 py-1.5 ml-4   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
+                                    {...register("twitter_link")}  
+                                />
+                            </div>
+                            
                         </div>
-                        <div className=' flex items-center py-3'>                            
-                            <img src="/src/assets/facebook.png " alt="" className='w-8 h-8' />
-                            <InputField 
-                                type="text"
-                                placeholder="Facebook URL"
-                                className="w-full px-4 py-1.5 ml-4   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
-                                {...register("facebook_link")}  
-                            />
-                        </div>
-                        <div className=' flex items-center py-3'>                            
-                            <img src="/src/assets/twitter.png " alt="" className='w-8 h-8' />
-                            <InputField 
-                                type="text"
-                                placeholder="Twitter URL"
-                                className="w-full px-4 py-1.5 ml-4   border  hover:border-[#1080e9]  focus:outline-none focus:border-1 focus:border-[#1080e9] rounded-md "
-                                {...register("twitter_link")}  
-                            />
-                        </div>
-                        
+
+                        <InputButton 
+                            type="submit" 
+                            content="Save"  
+                            className="w-24 bg-[#2F87FE] hover:bg-[#0570fc]   px-6 py-2  md:ml-1 my-5 text-white   rounded-sm  font-semibold text-xl " 
+                        />
                     </div>
-
-                    <InputButton 
-                        type="submit" 
-                        content="Save"  
-                        className="w-24 bg-[#2F87FE] hover:bg-[#0570fc]   px-6 py-2  md:ml-1 my-5 text-white   rounded-sm  font-semibold text-xl " 
-                    />
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     )
 }
--- Travel_Experience_Sharing_Platform/src/pages/Settings.jsx
+++ Travel_Experience_Sharing_Platform/src/pages/Settings.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
  
 function Settings(){
- 
+    const [passwordVisible,setPasswordVisible]=useState(false)
+    const [oldPassword,setOldPassword]=useState()
+    const [newPassword,setNewPassword]=useState()
+
     return (
-        <div>
-            <h2>settings component</h2> 
+        <div className=' pt-12  bg-[#F7F8FA]'>
+            <p className='text-xl  pb-8 px-8 md:px-24 text-[#757575] '>username/settings</p>
+            <div className='flex flex-col justify-center mx-6 sm:mx-auto sm:w-2/3 md:w-3/5 lg:w-1/2 xl:w-2/5  bg-white border py-6 px-4 '>
+                
+                <h1 className='border-b'>Change Password</h1>
+
+                <div  className="flex  px-2 m-2 border hover:border-[#1080e9]  rounded-sm">
+                    <input 
+                        type={passwordVisible? 'text':'password'} 
+                        placeholder="Enter Password" 
+                        className="w-full py-2 focus:outline-none "
+
+                    />
+                    <button
+                        type="button"
+                        className="w-8 pl-2"
+                        onClick={()=> setPasswordVisible(!passwordVisible)}
+                    >
+                        {passwordVisible ? (<img src="/src/assets/open.png" />) :(<img src="/src/assets/hide.png" />) }
+                    </button>
+                </div>
+
+            </div>
         </div>
     )
 }
